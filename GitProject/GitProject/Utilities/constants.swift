0af2809a7829eab526f8d91be302c4f8e7a2be8c
Nirha Patel <nrp001@ucsd.edu>
2017-12-04 15:56:58 -0800

    Lesson 4 Partial

---
+++ GitProject/GitProject/Utilities/constants.swift
@@ -0,0 +1,13 @@
+//
+//  constants.swift
+//  GitProject
+//
+//  Created by Nirha Patel on 04/12/17.
+//  Copyright © 2017 Nirha Patel. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+
+let TO_LOGIN = "toLogin"
