4a2f7fed107db1e028cf25495c2bf7e8a051eb5e
Nirha Patel <nrp001@ucsd.edu>
2017-12-07 16:22:42 -0800

    End of Lecture 4

---
+++ GitProject/GitProject/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  GitProject
+//
+//  Created by Nirha Patel on 07/12/17.
+//  Copyright © 2017 Nirha Patel. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
