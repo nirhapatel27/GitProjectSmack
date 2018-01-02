775f62d0e589118b2d137d47ac3b958b2d849db0
Nirha Patel <nrp001@ucsd.edu>
2018-01-03 00:10:35 +0530

    End of lesson 10

---
+++ GitProject/GitProject/View/RoundedButton.swift
@@ -0,0 +1,32 @@
+//
+//  RoundedButton.swift
+//  GitProject
+//
+//  Created by Nirha Patel on 03/01/18.
+//  Copyright © 2018 Nirha Patel. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 3.0
+        {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setupView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+    func setupView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+}
