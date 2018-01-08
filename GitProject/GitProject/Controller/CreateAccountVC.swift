c30101410c1630cb83681173fb3c0a1d1d712940
Nirha Patel <nrp001@ucsd.edu>
2018-01-08 07:13:12 -0800

    End of lesson 12

--- GitProject/GitProject/Controller/CreateAccountVC.swift
+++ GitProject/GitProject/Controller/CreateAccountVC.swift
@@ -12,10 +12,15 @@ class CreateAccountVC: UIViewController {
     
     //Outlets
     @IBOutlet weak var usernameTxt: UITextField!
-    
     @IBOutlet weak var emailTxt: UITextField!
     @IBOutlet weak var passTxt: UITextField!
     @IBOutlet weak var userImg: UIImageView!
+    
+    //Variables
+    var avatarName = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -29,6 +34,11 @@ class CreateAccountVC: UIViewController {
     }
     
     @IBAction func createAccntPressed(_ sender: Any) {
+        
+        guard let name = usernameTxt.text , usernameTxt.text != "" else {
+            return
+        }
+        
         guard let email = emailTxt.text , emailTxt.text != "" else {
             return
         }
@@ -41,7 +51,12 @@ class CreateAccountVC: UIViewController {
             if success {
                 AuthService.instance.loginUser(email: email, password: pass, completion: { (success) in
                     if success {
-                        print("logged in user!", AuthService.instance.authToken)
+                        AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
+                            if success{
+                                print(UserDataService.instance.name, UserDataService.instance.avatarName)
+                                self.performSegue(withIdentifier: UNWIND, sender: nil)
+                            }
+                        })
                     }
                 })
             }
