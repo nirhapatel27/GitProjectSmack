459be9d8dd68f5dea92883c7941767e43820848f
Nirha Patel <nrp001@ucsd.edu>
2018-01-09 23:08:11 -0800

    End of lesson 19

--- GitProject/GitProject/Model/Channel.swift
+++ GitProject/GitProject/Model/Channel.swift
@@ -9,8 +9,7 @@
 import Foundation
 
 struct Channel: Decodable {
-    public private(set) var _id: String!
-    public private(set) var name: String!
-    public private(set) var description: String!
-    public private(set) var __v: String!
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var id: String!
 }
--- GitProject/GitProject/Services/MessageService.swift
+++ GitProject/GitProject/Services/MessageService.swift
@@ -19,22 +19,18 @@ class MessageService{
         Alamofire.request(URL_GET_CHANNELS, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
             if response.result.error == nil{
                 guard let data = response.data else{return}
-                do{
-                    self.channels = try JSONDecoder().decode([Channel].self, from: data)
-                } catch let error {
-                    debugPrint(error as Any)
+
+                if let json = JSON(data: data).array{
+                    for item in json{
+                        let name = item["name"].stringValue
+                        let channelDescription = item["desription"].stringValue
+                        let id = item["_id"].stringValue
+                        let channel = Channel(channelTitle: name, channelDescription: channelDescription, id: id)
+                        self.channels.append(channel)
+                    }
+                    print(self.channels[0].channelTitle)
+                    completion(true)
                 }
-                print(self.channels)
-//                if let json = JSON(data: data).array{
-//                    for item in json{
-//                        let name = item["name"].stringValue
-//                        let channelDescription = item["desription"].stringValue
-//                        let id = item["_id"].stringValue
-//                        let channel = Channel(channelTitle: name, channelDescription: channelDescription, id: id)
-//                        self.channels.append(channel)
-//                    }
-//                    completion(true)
-//                }
 
             }else{
                 completion(false)
