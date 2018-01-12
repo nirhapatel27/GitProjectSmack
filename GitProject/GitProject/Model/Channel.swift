1cc8ee4aa312e9e7b950f374ab04294f5d5ce8db
Nirha Patel <nrp001@ucsd.edu>
2018-01-12 06:54:44 -0800

    End of lesson 22

--- GitProject/GitProject/Model/Channel.swift
+++ GitProject/GitProject/Model/Channel.swift
@@ -14,3 +14,4 @@ struct Channel : Decodable {
     public private(set) var id: String!
 }
 
+
---
+++ GitProject/GitProject/Model/Message.swift
@@ -0,0 +1,19 @@
+//
+//  Message.swift
+//  GitProject
+//
+//  Created by Nirha Patel on 11/01/18.
+//  Copyright © 2018 Nirha Patel. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    public private(set) var message: String!
+    public private(set) var userName: String!
+    public private(set) var channelId: String!
+    public private(set) var userAvatar: String!
+    public private(set) var userAvatarColor: String!
+    public private(set) var id: String!
+    public private(set) var timeStamp: String!
+}
